486747e53bc6736d5f48a618cca388ef4362f9b3
sgrimaux <118748194+sgrimaux@users.noreply.github.com>
2022-11-21 20:45:55 -0300

    Update lineas.py

--- lineas.py
+++ lineas.py
@@ -6,11 +6,8 @@ cv2.imshow('Imagen original', imgOrig)
 cv2.waitKey()
 
 gris = cv2.cvtColor(imgOrig, cv2.COLOR_BGR2GRAY)
-cv2.imshow('Imagen En grises',gris)
-cv2.waitKey()
 
 bordes = cv2.Canny(gris, 150, 500, apertureSize = 3, L2gradient=False)
-
 lineas = cv2.HoughLinesP(bordes, 1, nump.pi/180, 40, minLineLength=0, maxLineGap=60)
 
 if lineas is not None:
@@ -23,4 +20,3 @@ cv2.imshow('Bordes de Imagen limpia', bordes)
 cv2.waitKey()
 cv2.imshow('Lineas detectadas en la imagen original', imgOrig)
 cv2.waitKey()
-
